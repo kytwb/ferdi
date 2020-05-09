c92a0edbb80541626221f672f85a74891559edd5
romain <rpsa@pm.me>
2020-05-09 13:52:53 +0200

    Update 'About' title, header and message

--- src/components/settings/supportFerdi/SupportFerdiDashboard.js
+++ src/components/settings/supportFerdi/SupportFerdiDashboard.js
@@ -4,7 +4,7 @@ import { defineMessages, intlShape } from 'react-intl';
 const messages = defineMessages({
   headline: {
     id: 'settings.supportFerdi.headline',
-    defaultMessage: '!!!Support Ferdi',
+    defaultMessage: '!!!About Ferdi',
   },
   title: {
     id: 'settings.supportFerdi.title',
@@ -37,29 +37,54 @@ class SupportFerdiDashboard extends Component {
               <a href="https://opencollective.com/getferdi#section-contributors" target="_blank"><img alt="Open Collective sponsors" src="https://img.shields.io/opencollective/sponsors/getferdi?logo=open-collective" /></a>
             </p>
             <p>
-Ferdi is an open-source and a community-lead application.
-              <br />
-Thank to the people who make this possible:
+                Ferdi is an open-source and a community-lead application.
+            </p>
+            <p>
+                Thanks to the people who make this possbile:
               <br />
               <br />
             </p>
             <p><a href="#contributors-via-opencollective"><img alt="GitHub contributors (non-exhaustive)" width="100%" src="https://opencollective.com/getferdi/contributors.svg?width=642&button=false" /></a></p>
             <p>
-Full list of contributors
+              Full list of contributors
               <a href="https://github.com/getferdi/ferdi#contributors-" target="_blank" className="link">
                 {' '}
-here
+              here
                 <i className="mdi mdi-open-in-new" />
               </a>
               <br />
               <br />
             </p>
-            <p>The development of the app is done by volunteers. People who use the app like you. They maintain, fix, and improve Ferdi in their spare time.</p>
-            <p>Support is always welcomed; and while volunteers do most of the work for free, we need to pay for servers and certificates.</p>
-            <p>As a community, we are fully transparent on funds we collect and spend. You can see our financial accounts on Open Collective</p>
+            <p>The development of Ferdi is done by volunteers. People who use Ferdi like you. They maintain, fix, and improve Ferdi in their spare time.</p>
             <p>
-If you feel like supporting Ferdi with a donation, you can do so. It will be greatly appreciated.
-              <br />
+              Support is always welcome. You can find a list of the help we need
+              <a href="https://help.getferdi.com/general/support" target="_blank" className="link">
+                {' '}
+              here
+                <i className="mdi mdi-open-in-new" />
+              </a>
+            </p>
+            <p>
+            While volunteers do most of the work, we still need to pay for servers and certificates. As a community, we are fully transparent on funds we collect and spend. You can see our financial accounts on
+              <a href="https://opencollective.com/getferdi#section-budget" target="_blank" className="link">
+                {' '}
+                Open Collective
+                <i className="mdi mdi-open-in-new" />
+              </a>
+            </p>
+            <p>
+              If you feel like supporting Ferdi with a donation, you can do so
+              <a href="https://opencollective.com/getferdi#section-budget" target="_blank" className="link">
+                {' '}
+                here
+                <i className="mdi mdi-open-in-new" />
+              </a>
+            </p>
+            <p>
+                  Your help will be greatly appreciated.
+            </p>
+            <p>
+              Thank you for supporting Ferdi.
             </p>
           </div>
         </div>
