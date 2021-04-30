585b9dd5fdd1333edd787160c988fada9b5a7209
Vijay A <avijayr@protonmail.com>
2021-04-30 22:32:21 +0530

    Quick Switch now supports searching using regular expressions.

--- src/features/quickSwitch/Component.js
+++ src/features/quickSwitch/Component.js
@@ -8,6 +8,7 @@ import { defineMessages, intlShape } from 'react-intl';
 import { Input } from '@meetfranz/forms';
 import { H1 } from '@meetfranz/ui';
 
+import { compact, invoke } from 'lodash';
 import Modal from '../../components/ui/Modal';
 import { state as ModalState } from '.';
 import ServicesStore from '../../stores/ServicesStore';
@@ -134,10 +135,10 @@ export default @injectSheet(styles) @inject('stores', 'actions') @observer class
   // Get currently shown services
   services() {
     let services = [];
-    if (this.state.search) {
+    if (this.state.search && compact(invoke(this.state.search, 'match', /^[a-z0-9]/i)).length > 0) {
       // Apply simple search algorythm to list of all services
       services = this.props.stores.services.allDisplayed;
-      services = services.filter(service => service.name.toLowerCase().includes(this.state.search.toLowerCase()));
+      services = services.filter(service => service.name.toLowerCase().search(this.state.search.toLowerCase()) !== -1);
     } else {
       // Add last used services to services array
       for (const service of this.props.stores.services.lastUsedServices) {
