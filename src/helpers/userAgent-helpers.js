bb41e0615a1b25650a8f47e229ef8878f8b8c31c
Vijay A <avijayr@protonmail.com>
2021-07-03 15:35:56 +0530

    Use 'useragent-generator' to generate the user-agent string rather than manually building it.

--- src/helpers/userAgent-helpers.js
+++ src/helpers/userAgent-helpers.js
@@ -1,50 +1,7 @@
-import os from 'os';
-import macosVersion from 'macos-version';
-import {
-  ferdiVersion, electronVersion, chromeVersion, isMac, isWindows,
-} from '../environment';
+import { chromeVersion } from '../environment';
 
-function macOS() {
-  const version = macosVersion();
-  return `Macintosh; Intel Mac OS X ${version.replace(/\./g, '_')}`;
-}
+const uaGenerator = require('useragent-generator');
 
-function windows() {
-  const version = os.release();
-  const [majorVersion, minorVersion] = version.split('.');
-  return `Windows NT ${majorVersion}.${minorVersion}; Win64; x64`;
-}
-
-function linux() {
-  return 'X11; Ubuntu; Linux x86_64';
-}
-
-export function isChromeless(url) {
-  return url.startsWith('https://accounts.google.com');
-}
-
-export default function userAgent(removeChromeVersion = false, addFerdiVersion = false) {
-  let platformString = '';
-
-  if (isMac) {
-    platformString = macOS();
-  } else if (isWindows) {
-    platformString = windows();
-  } else {
-    platformString = linux();
-  }
-
-  let chromeVersionString = 'Chrome';
-  if (!removeChromeVersion) {
-    chromeVersionString = `Chrome/${chromeVersion}`;
-  }
-
-  let applicationString = '';
-  if (addFerdiVersion) {
-    applicationString = ` Ferdi/${ferdiVersion} Electron/${electronVersion}`;
-  }
-
-  // Chrome is pinned to WebKit 537.36, the latest version before hard forking to Blink.
-  return `Mozilla/5.0 (${platformString}) AppleWebKit/537.36 (KHTML, like Gecko) ${chromeVersionString} Safari/537.36${applicationString}`;
-  // Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36 Ferdi/5.5.1-nightly.13 Electron/8.2.3
+export default function userAgent() {
+  return uaGenerator.chrome(chromeVersion);
 }
